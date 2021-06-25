cf3c253ec25c8c742fa69f0be5708e22b5029844
Paul K Lynch <dramatichamster@gmail.com>
2021-06-25 20:03:11 +1000

    Trying out some things

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const gridWidth = 8
+  const gridHeight = 3
+  // let grid = new Array(gridWidth * gridHeight).map(() => { console.log('x'); return 1 })
+  let grid = Array.from(Array(gridWidth * gridHeight).keys())
+
+
+  console.log('grid', grid, getCellNeighbours(grid, 23, gridWidth)) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test2
     </div>
-  );
+  )
+}
+
+const getCellNeighbours = (grid: Array<number>, cellIndex: number, width: number): Array<number> => {
+  let neighbours: Array<number> = []
+
+  for (let rowOffset = -1; rowOffset < 2; rowOffset++) {
+    const rowIndex = cellIndex + (rowOffset * width);
+
+    if (rowIndex >= 0 && rowIndex < grid.length) {
+      const rowNum = Math.floor(rowIndex / width);
+      const rowMin = width * rowNum;
+      const rowMax = rowMin + (width - 1);
+ 
+      for(let colOffset = -1; colOffset < 2; colOffset++) {
+        const neighbourIndex = rowIndex + colOffset;
+        
+        if (
+          neighbourIndex >= rowMin // Must be equal or greater than row min
+          && neighbourIndex <= rowMax // Must be equal or less than row max
+          && neighbourIndex !== cellIndex // Can't be current cell index
+        ) {
+          neighbours.push(neighbourIndex)
+        }
+      }
+    }
+
+  }
+
+
+  return neighbours
 }
 
 export default App;
